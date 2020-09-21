46e5c8e39095370bfce668e178888e6934262554
砂漠の毒虫 <853419196@qq.com>
2020-09-22 02:31:13 +0900

    Delete CommonJS.js

--- src/CommonJS.js
+++
@@ -1,6 +0,0 @@
-function require(filename)
-{
-    const module={exports:{}};
-    Function("exports","module",read(filename)).call(module.exports,module.exports,module);
-    return module.exports;
-}
