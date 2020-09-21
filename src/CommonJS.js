39893de79553d664ad0348c156d8d226b152873f
砂漠の毒虫 <853419196@qq.com>
2020-09-21 23:47:52 +0900

    Add files via upload

---
+++ src/CommonJS.js
@@ -0,0 +1,7 @@
+"use strict";
+function require(filename)
+{
+    const module={exports:{}};
+    Function("exports","module",read(filename)).call(module.exports,module.exports,module);
+    return module.exports;
+}
